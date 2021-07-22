78f04704945df97b8a18656577ecb11ee6b67382
rigobertoalag <a20150338@utem.edu.mx>
2021-07-22 13:02:16 -0500

    Config styles by user

--- src/stylesConfig.js
+++ src/stylesConfig.js
@@ -1,5 +1,5 @@
 export const globalStyles = {
-    "header" : "linear-gradient(to right, #0f0c29, #302b63, #24243e);",
+    "header" : "#0278a1",
     "HLogo" : "#fff",
     "HoriCatBody": "#efeff1",
     "OfferLabel" : "linear-gradient(to right, #d31027, #ea384d);",
